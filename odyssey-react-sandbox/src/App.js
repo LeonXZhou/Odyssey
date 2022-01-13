9c59bc17807a8f1b676c4dbb8b78ed104f5f4cca
LeonXZhou <leon.zhou@queensu.ca>
2022-01-12 17:10:52 -0800

    refactored listeners a bit for map

--- odyssey-react-sandbox/src/App.js
+++ odyssey-react-sandbox/src/App.js
@@ -8,13 +8,12 @@ import L from 'leaflet';
 function LocationMarker() {
   const [markerPosition, setPosition] = useState([])
   const map = useMapEvents({
-    click(e) {
+    click: (e) => {
       console.log(e)
       setPosition((prev) => { return [...prev, [e.latlng.lat, e.latlng.lng]] })
-    }
+    },
   })
-  console.log(markerPosition)
-  const markers = markerPosition.map((mP,i) => {
+  const markers = markerPosition.map((mP, i) => {
     return (<Marker position={mP} icon={new L.Icon({
       iconUrl: "https://i.imgur.com/LpaY82x.png",
       iconAnchor: null,
@@ -25,7 +24,7 @@ function LocationMarker() {
       iconSize: new L.Point(20, 20),
       className: 'leaflet-div-icon'
     })}
-    key={i}></Marker>)
+      key={i}></Marker>)
   })
   return (<>{markers}</>)
 }
@@ -38,20 +37,20 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]} icon={new L.Icon({
-          iconUrl: "https://i.imgur.com/LpaY82x.png",
-          iconAnchor: null,
-          popupAnchor: [0, 0],
-          shadowUrl: null,
-          shadowSize: null,
-          shadowAnchor: null,
-          iconSize: new L.Point(20, 20),
-          className: 'leaflet-div-icon'
-        })}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+          <Marker position={[51.505, -0.09]} icon={new L.Icon({
+            iconUrl: "https://i.imgur.com/LpaY82x.png",
+            iconAnchor: null,
+            popupAnchor: [0, 0],
+            shadowUrl: null,
+            shadowSize: null,
+            shadowAnchor: null,
+            iconSize: new L.Point(20, 20),
+            className: 'leaflet-div-icon'
+          })}>
+            <Popup>
+              Popup Text
+            </Popup>
+          </Marker>
         <LocationMarker></LocationMarker>
       </MapContainer>
     </div>
