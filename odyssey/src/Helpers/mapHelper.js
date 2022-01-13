2bd358a18b755a02bc4a1681afcb4a92df8832bb
LeonXZhou <leon.zhou@queensu.ca>
2022-01-12 23:02:09 -0800

    added comments

--- odyssey/src/Helpers/mapHelper.js
+++ odyssey/src/Helpers/mapHelper.js
@@ -1,6 +1,6 @@
-//maps icon string to icon url
-export function themeAttributionFinder(iconString) {
-  switch (iconString) {
+//maps themeAttribution string
+export function themeAttributionFinder(themeString) {
+  switch (themeString) {
     case "TOPO":
       return 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     default:
@@ -8,8 +8,9 @@ export function themeAttributionFinder(iconString) {
   }
 }
 
-export function themeURLFinder(iconString) {
-  switch (iconString) {
+//maps themeURL String
+export function themeURLFinder(themeString) {
+  switch (themeString) {
     case "TOPO":
       return 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
     default:
