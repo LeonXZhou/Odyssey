67eb7eecb243dd21b6ed1fc5fba42f8c8e4ab4b1
LeonXZhou <leon.zhou@queensu.ca>
2022-01-14 01:18:52 +0000

    fixed double quotes

--- node-skeleton/db/seeds/05_days.sql
+++ node-skeleton/db/seeds/05_days.sql
@@ -1,7 +1,6 @@
 -- Widgets table seeds here (Example)
-INSERT INTO days (trip_id, date) VALUES (1,"2022-01-08");
-INSERT INTO days (trip_id, date) VALUES (1,"2022-01-09");
-
-INSERT INTO days (trip_id, date) VALUES (2,"2022-01-08");
-INSERT INTO days (trip_id, date) VALUES (2,"2022-01-09");
-INSERT INTO days (trip_id, date) VALUES (2,"2022-01-10");
+INSERT INTO days (trip_id, date) VALUES (1,'2022-01-08');
+INSERT INTO days (trip_id, date) VALUES (1,'2022-01-09');
+INSERT INTO days (trip_id, date) VALUES (2,'2022-01-08');
+INSERT INTO days (trip_id, date) VALUES (2,'2022-01-09');
+INSERT INTO days (trip_id, date) VALUES (2,'2022-01-10');
--- node-skeleton/db/seeds/09_gear_categories.sql
+++ node-skeleton/db/seeds/09_gear_categories.sql
@@ -1,3 +1,3 @@
-INSERT INTO gear_categories (trip_id, date,name) VALUES (1,"Apparel");
-INSERT INTO gear_categories (trip_id, date,name) VALUES (1,"Shelter/Sleep");
-INSERT INTO gear_categories (trip_id, date,name) VALUES (1,"Cooking Gear");
+INSERT INTO gear_categories (trip_id,name) VALUES (1,'Apparel');
+INSERT INTO gear_categories (trip_id,name) VALUES (1,'Shelter/Sleep');
+INSERT INTO gear_categories (trip_id,name) VALUES (1,'Cooking Gear');
