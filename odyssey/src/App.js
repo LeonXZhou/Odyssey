4945dd1f502725ab2f7bf7b23411a617813ebeaa
Scott Hillier <scotthhillier@gmail.com>
2022-01-14 12:30:20 -0800

    resolved conflicts

--- odyssey/src/App.js
+++ odyssey/src/App.js
@@ -1,40 +1,25 @@
-<<<<<<< HEAD
-import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import Navigation from "./components/Navigation";
-import DisplayTrips from "./components/DisplayTrips";
-import AddTrip from "./components/AddTrip";
-
-const App = () => {
-  return (
-    <div className="App">
-      <Navigation />
-      <Home></Home>
-=======
-import { Link } from 'react-router-dom';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <h1>always here</h1>
-        <Link to='/'>home</Link>
+        <Link to="/">home</Link>
         <br />
-        <Link to='/planning'>planning</Link>
+        <Link to="/planning">planning</Link>
         <br />
-        <Link to='/asdf'>asdf</Link>
-        
+        <Link to="/asdf">asdf</Link>
+
         <Routes>
           <Route path="/" element={<h1>this is: home</h1>} />
           <Route path="/planning" element={<h1>this is: planning</h1>} />
           <Route path="/asdf" element={<h1>this is: asdf</h1>} />
         </Routes>
       </BrowserRouter>
->>>>>>> main
     </div>
   );
-};
+}
 
 export default App;
