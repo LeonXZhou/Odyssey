e4953e29b6125f50081d7a7c38699841756361a7
Scott Hillier <scotthhillier@gmail.com>
2022-01-13 16:56:35 -0800

    added seed files for database

---
+++ node-skeleton/db/seeds/02_trips.sql
@@ -0,0 +1,4 @@
+INSERT INTO trips (name, description, start_date, end_date)
+VALUES
+  ('Small Hike', 'Small hike up mountain', '2022-01-03', '2022-01-04'),
+  ('Big Hike', 'Big hike up mountain', '2021-11-07', '2021-11-09');
