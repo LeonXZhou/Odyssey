9d436b6d4e6be683c3bda81f5df002310f485dc8
LeonXZhou <leon.zhou@queensu.ca>
2022-01-14 00:56:03 +0000

    fixed meals and meal items

--- node-skeleton/db/seeds/08_meal_items.sql
+++ node-skeleton/db/seeds/08_meal_items.sql
@@ -1,6 +1,6 @@
 INSERT INTO meal_items (meal_id, name,quantity) VALUES (1,"brocoli","10 cups");
 INSERT INTO meal_items (meal_id, name,quantity) VALUES (1,"brocoli","10 cups");
-INSERT INTO meal_items (meal_id, name,quantity) VALUES (1,"brocoli","10 cups");
-
-
 INSERT INTO meal_items (meal_id, name,quantity) VALUES (2,"brocoli","10 cups");
+
+
+INSERT INTO meal_items (meal_id, name,quantity) VALUES (3,"brocoli","10 cups");
