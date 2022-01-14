afa61a3a60157ad9febe8f39241dabd45c6a8ffe
LeonXZhou <leon.zhou@queensu.ca>
2022-01-14 00:56:03 +0000

    fixed meals and meal items

--- node-skeleton/db/seeds/07_meals.sql
+++ node-skeleton/db/seeds/07_meals.sql
@@ -1,3 +1,3 @@
-INSERT INTO meals (day_id, date,name) VALUES (1,"2022-01-08","breakfast");
-INSERT INTO meals (day_id, date,name) VALUES (2,"2022-01-09","lunch");
-INSERT INTO meals (day_id, date,name) VALUES (3,"2022-01-10","dinner");
+INSERT INTO meals (day_id, date,name) VALUES (1,"breakfast");
+INSERT INTO meals (day_id, date,name) VALUES (1,"lunch");
+INSERT INTO meals (day_id, date,name) VALUES (1,"dinner");
