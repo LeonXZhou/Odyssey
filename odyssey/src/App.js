7aa90afe9721b0a1cb7eafcd36dc40d9b789d887
Scott Hillier <scotthhillier@gmail.com>
2022-01-14 14:10:09 -0800

    home page able to pass information, pulling main

--- odyssey/src/App.js
+++ odyssey/src/App.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <h1>always here</h1>
+        <Navigation />
         <Link to="/">home</Link>
         <br />
         <Link to="/planning">planning</Link>
@@ -13,7 +16,7 @@ function App() {
         <Link to="/asdf">asdf</Link>
 
         <Routes>
-          <Route path="/" element={<h1>this is: home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/planning" element={<h1>this is: planning</h1>} />
           <Route path="/asdf" element={<h1>this is: asdf</h1>} />
         </Routes>
