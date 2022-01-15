3cc99c0ce259cf80c53844dab03672f9e675927d
Scott Hillier <scotthhillier@gmail.com>
2022-01-15 13:17:50 -0800

    initial planning page finished

---
+++ odyssey/src/components/planning/Equipment.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../component-styles/Planning.scss";
+
+const Sidebar = () => {
+  return <main className="equipment">THIS IS EQUIPMENT</main>;
+};
+
+export default Sidebar;
