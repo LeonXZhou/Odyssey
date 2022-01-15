60610f43c31443291517745d25191546dbc04eae
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-14 18:19:08 -0700

    currently working on planTripsEquipment

--- node-skeleton/db/queries/03_plan_trip_equipment.sql
+++ node-skeleton/db/queries/03_plan_trip_equipment.sql
@@ -1,4 +1,7 @@
-SELECT gear_items.name AS gear_item, gear_categories.name AS catergory
+SELECT gear_items.name AS gear_item, gear_categories.name AS catergory , trips.id , trips.name
 FROM gear_items
 JOIN gear_categories on gear_items.gear_category_id=gear_categories.id
-where gear_categories.id = 1;
+JOIN trips ON gear_categories.trip_id = trips.id
+;
+
+-- { tripid: trip_id , tripsName: trip_name,gear_categories.name: [{gearName  : gear_item.name }]}
--- node-skeleton/server.js
+++ node-skeleton/server.js
@@ -36,13 +36,15 @@ app.use(
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-const tripMealsRoutes = require("./routes/plan-trips-meals");
+const tripMealsRoutes = require("./routes/planTripsMeals");
+const tripEquipmentRoutes = require("./routes/planTripsEquipment");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/plan-trips-meals", tripMealsRoutes(db));
+app.use("/api/plan-trips-equipment", tripEquipmentRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
