98e33e5afde9493987567a8a8c439a887f3c57e5
Scott Hillier <scotthhillier@gmail.com>
2022-01-14 18:07:26 -0800

    pushing for bug fix

--- odyssey/src/Helpers/markerHelpers.js
+++ odyssey/src/Helpers/markerHelpers.js
@@ -9,6 +9,9 @@ export function iconFinder(iconString) {
 
     case "TENT":
       return "https://www.pngfind.com/pngs/m/80-807696_png-file-svg-camping-tent-svg-transparent-png.png";
+
+    default:
+      return "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png";
   }
 }
 
