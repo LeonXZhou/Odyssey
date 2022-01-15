dd663d771c6a0f3c2ef68b389101f1dcc183441c
Scott Hillier <scotthhillier@gmail.com>
2022-01-15 14:01:10 -0800

    pushing into main

--- odyssey/src/components/planning/Meals.js
+++ odyssey/src/components/planning/Meals.js
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../component-styles/Planning.scss";
 
-const Sidebar = () => {
+const Meals = () => {
   return <main className="equipment">THIS IS MEALS</main>;
 };
 
-export default Sidebar;
+export default Meals;
