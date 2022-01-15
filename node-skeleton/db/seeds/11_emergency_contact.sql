cb5c582976319170ecb4a14b6cd12ae0adf21906
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-15 15:42:58 -0700

    fixing moduels

--- node-skeleton/db/seeds/11_emergency_contact.sql
+++ node-skeleton/db/seeds/11_emergency_contact.sql
@@ -1,4 +1,4 @@
 --
 INSERT INTO emergency_contacts (name,phone_number,email,send_date,trip_id) VALUES ('jeff','123456789','h@h.com',current_timestamp,1);
-INSERT INTO emergency_contacts (name,phone_number,email,send_date,trip_id) VALUES ('lorenzo','987654321','ho@ho.com',current_timestamp,1);
-INSERT INTO emergency_contacts (name,phone_number,email,send_date,trip_id) VALUES ('steeve','1234598765','he@he.com',current_timestamp,1);
+INSERT INTO emergency_contacts (name,phone_number,email,send_date,trip_id) VALUES ('lorenzo','987654321','ho@ho.com',current_timestamp,1,false);
+INSERT INTO emergency_contacts (name,phone_number,email,send_date,trip_id) VALUES ('steeve','1234598765','he@he.com',current_timestamp,1,true);
