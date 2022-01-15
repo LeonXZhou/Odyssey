aaaa7f6d9343810e84a7c07b092f12f0c19b7c90
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-14 19:12:52 -0700

    added meals

---
+++ node-skeleton/routes/meals.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT  meals.id AS meals_id ,meals.name AS meal, meal_items.id AS meal_itemID,meal_items.name AS food_item, meal_items.quantity, days.date AS days_date , days.id AS days_id
+    FROM meal_items
+    JOIN meals on meal_items.meal_id=meals.id
+    JOIN days on meals.day_id=days.id
+    ;
+    `)
+      .then(data => {
+        const allMeals = data.rows;
+        console.log(allMeals)
+        res.json(allMeals);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
