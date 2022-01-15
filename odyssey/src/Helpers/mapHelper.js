ea9a5edd565f96de32961fc06da74ef818da5e3a
LeonXZhou <leon.zhou@queensu.ca>
2022-01-14 17:31:54 -0800

    refactored code to use helper functions to generate markers and mapoptions insead

--- odyssey/src/Helpers/mapHelper.js
+++ odyssey/src/Helpers/mapHelper.js
@@ -18,3 +18,34 @@ export function themeURLFinder(themeString) {
 
   }
 }
+
+
+// mapOptions={
+//   zoom: 10,
+//   center: [49.246292, -123.116226],
+//   themeAttribution: "TOPO", //needed for theme: converted to url by helper functions in mapHelpers.js
+//   themeURL: "TOPO", //needed for theme: converted to url by helper functions in mapHelpers.js
+//   //route_id: we can pass in the id from the db later
+// }
+export function parseDBMap(tripMaps) {
+  return { center: [tripMaps.lat, tripMaps.long] }
+}
+
+
+// markers=[
+//   { position: [49.246292, -123.116226], iconSize: [40, 40] },
+//   {
+//     position: [49.286292, -123.136226],
+//     icon: "TENT",
+//     iconSize: [20, 20],
+//   }, //icon string is converted to actual url like the theme strings
+//   {
+//     position: [49.346292, -123.166226],
+//     icon: "TENT",
+//     iconSize: [20, 20],
+//     popUp: { name: "First Night", description: "I <3 Camping" },
+//   },
+// }
+export function parseDBMarkers(tripMarkers) {
+  return tripMarkers.map((serverMarker) => { return { position: [serverMarker.lat, serverMarker.long], icon: serverMarker.type } })
+}
