c8eb7147e6582b49a300b76f418676c64eed5e15
LeonXZhou <leon.zhou@queensu.ca>
2022-01-15 14:31:36 -0800

    implementing meal

--- odyssey/src/components/planning/MealDay.js
+++ odyssey/src/components/planning/MealDay.js
@@ -1,6 +1,14 @@
 import React from "react";
-const MealDay = () => {
-  return (<div>asdfasdfdasdfasdf</div>);
+import "../component-styles/MealDay.scss";
+import MealCard from "./MealCard";
+
+const MealDay = (props) => {
+  return (
+    <div className={"mealDay"}>
+      <div>Day 1</div>
+      <MealCard></MealCard>
+      <MealCard></MealCard>
+    </div>);
 };
 
 export default MealDay;
