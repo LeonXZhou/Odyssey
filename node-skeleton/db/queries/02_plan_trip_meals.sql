e63470131cfaf4983cf81ddfcd998de77e06c6a7
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-14 17:41:47 -0700

    added mealsHelper.js

--- node-skeleton/db/queries/02_plan_trip_meals.sql
+++ node-skeleton/db/queries/02_plan_trip_meals.sql
@@ -1,4 +1,10 @@
-SELECT meals.name AS meal, meal_items.name AS food_item, meal_items.quantity
+-- days ,meals, meal_items 
+
+
+SELECT  meals.id AS meals_id ,meals.name AS meal, meal_items.id AS meal_itemID,meal_items.name AS food_item, meal_items.quantity, days.date AS days_date , days.id AS days_id
 FROM meal_items
 JOIN meals on meal_items.meal_id=meals.id
-WHERE meals.id =1;
+JOIN days on meals.day_id=days.id
+;
+
+-- { day: day_id , meals: [{meal_name  : meal_name ,meal.id , meal_items: [{meals_itemname: meal_items.name, meal_items.id mealsquantity: meal_items.quantity}]}]}
