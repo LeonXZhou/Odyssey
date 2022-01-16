e2579b5b7809eda0e6b9eb81de1f51e650f15608
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-16 13:41:28 -0700

    added quantity to gear_items table

--- node-skeleton/db/schema/10_gear_items.sql
+++ node-skeleton/db/schema/10_gear_items.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS gear_items CASCADE;
 CREATE TABLE gear_items (
   id SERIAL PRIMARY KEY NOT NULL,
   gear_category_id INTEGER REFERENCES gear_categories(id),
-  name VARCHAR(255)
+  name VARCHAR(255),
+  quantity INTEGER
 );
