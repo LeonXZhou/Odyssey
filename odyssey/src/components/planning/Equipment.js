4d34d4f4cbe96f7ec97ea416dacdab25565277ba
Scott Hillier <scotthhillier@gmail.com>
2022-01-15 16:04:39 -0800

    props can be passed down to individual equipment categories

--- odyssey/src/components/planning/Equipment.js
+++ odyssey/src/components/planning/Equipment.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "../component-styles/Equipment.scss";
+import EquipmentItem from "./EquipmentItem";
 
 const Equipment = () => {
-  // const equipment = equipmentArray.map((equipment, i) => {
-  //   return <EquipmentItem key={i} name={equipment.name} />;
-  // });
+  const equipmentArray = [
+    {
+      apparel: {
+        item1: "jacket",
+        item2: "socks",
+      },
+    },
+    {
+      apparel: {
+        item1: "jacket",
+        item2: "socks",
+      },
+    },
+  ];
+  const EquipmentItems = equipmentArray.map((category, i) => {
+    return <EquipmentItem key={i} apparel={category.apparel} />;
+  });
   return (
     <main className="equipment">
+      {EquipmentItems}
       <div className="equipment-card">
-        <div className="descriptionContainer">
-          <h1>
-            <b>Apparel</b>
-          </h1>
-          <p>{"props.description"}</p>
-          <p>Created By: {"props.username"}</p>
+        <div className="equipment-card-header">
+          <h3>Add</h3>
         </div>
       </div>
     </main>
