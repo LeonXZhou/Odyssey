2439fd3a23a42ffed9b4e4ca811a242547839884
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-16 14:02:32 -0700

    updated helpers equipment with quantity

--- node-skeleton/routes/equipment.js
+++ node-skeleton/routes/equipment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/:trip_id", (req, res) => {
-    const query = `SELECT gear_items.name AS gear_item, gear_categories.name AS catergory , trips.id , trips.name
+    const query = `SELECT gear_items.name AS gear_item, gear_items.quantity,gear_categories.name AS catergory, trips.id , trips.name
     FROM gear_items
     JOIN gear_categories on gear_items.gear_category_id=gear_categories.id
     JOIN trips ON gear_categories.trip_id = trips.id
@@ -11,7 +11,7 @@ module.exports = (db) => {
     const values = [req.params.trip_id];
     db.query(query, values)
       .then((req) => {
-        const allTrip = req.rows;
+      
         res.json(req.rows)
       })
       .catch((err) => {
