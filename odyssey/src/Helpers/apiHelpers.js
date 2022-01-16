1c93cd492772e002487bc9a64f51e7e696a56bad
Scott Hillier <scotthhillier@gmail.com>
2022-01-16 13:00:06 -0800

    pulling main

--- odyssey/src/Helpers/apiHelpers.js
+++ odyssey/src/Helpers/apiHelpers.js
@@ -4,14 +4,10 @@ export function getTrips() {
   return axios.get("/api/trips");
 }
 
-<<<<<<< HEAD
-export function getMealsForTrip() {}
-=======
 export function getMapForTrip(trip_id) {
-  return axios.get(`/api/trips/${trip_id}`)
+  return axios.get(`/api/trips/${trip_id}`);
 }
 
-export function getEquipmentForTrip(trip_id){
-  return axios.get(`/api/equipment/${trip_id}`)
+export function getEquipmentForTrip(trip_id) {
+  console.log(axios.get(`/api/equipment/${trip_id}`));
 }
->>>>>>> main
--- odyssey/src/components/planning/Equipment/Equipment.js
+++ odyssey/src/components/planning/Equipment/Equipment.js
@@ -2,27 +2,14 @@ import React from "react";
 import "../../component-styles/Equipment.scss";
 import EquipmentItem from "./EquipmentItem";
 
-const Equipment = () => {
-  const equipmentArray = [
-    {
-      apparel: {
-        item1: "jacket",
-        item2: "socks",
-      },
-    },
-    {
-      apparel: {
-        item1: "jacket",
-        item2: "socks",
-      },
-    },
-  ];
-  const EquipmentItems = equipmentArray.map((category, i) => {
-    return <EquipmentItem key={i} apparel={category.apparel} />;
-  });
+const Equipment = (props) => {
+  console.log("EQUIPMENT ARRAY: ", props.equipmentArray);
+  // const EquipmentItems = equipmentArray.map((category, i) => {
+  //   return <EquipmentItem key={i} apparel={category.apparel} />;
+  // });
   return (
     <main className="equipment">
-      {EquipmentItems}
+      {/* {EquipmentItems} */}
       <div className="equipment-card">
         <div className="equipment-card-header">
           <h3>Add</h3>
