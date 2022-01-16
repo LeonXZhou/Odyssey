d5ccf57449e4c191c926d0aa59d4d1b1ef24b202
LeonXZhou <leon.zhou@queensu.ca>
2022-01-15 19:17:07 -0800

    implemented viewing based on trip id

---
+++ odyssey/src/components/TripDisplayItemLink.js
@@ -0,0 +1,38 @@
+import "leaflet/dist/leaflet.css";
+import "./component-styles/TripDisplayItem.scss";
+import { generateMarkers } from "../Helpers/markerHelpers";
+import { themeAttributionFinder, themeURLFinder } from "../Helpers/mapHelper";
+import { Link } from "react-router-dom";
+import MapDisplay from "./MapComponents/MapDisplay";
+function TripDisplayItemLink(props) {
+  let markers = <></>;
+
+  if (props.markers) {
+    markers = generateMarkers(props.markers);
+  }
+
+  //Defaulting Map Theme
+  const mapThemeAttribution =
+    props.mapOptions &&
+    themeAttributionFinder(props.mapOptions.themeAttribution);
+  const mapThemeURL =
+    props.mapOptions && themeURLFinder(props.mapOptions.themeAttribution);
+
+  return (
+    <Link className={"tripDisplayCard"} to={`/planning/view/${props.trip_id}`}>
+      <MapDisplay
+        mapOptions={props.mapOptions}
+        markers={props.markers}
+      ></MapDisplay>
+      <div className="descriptionContainer">
+        <h1>
+          <b>{props.name}</b>
+        </h1>
+        <p>{props.description}</p>
+        <p>Created By: {props.username}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default TripDisplayItemLink;
