315b938b736502fe128ca007c7b2bf33007dea87
LeonXZhou <leon.zhou@queensu.ca>
2022-01-17 13:40:09 -0500

    implemented a dev login page at /login/dev

--- node-skeleton/routes/authentication.js
+++ node-skeleton/routes/authentication.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { reservationsUrl } = require('twilio/lib/jwt/taskrouter/util');
+const route = require('color-convert/route');
 
 const insertUser = (db, user) => {
     const query = `INSERT INTO users (first_name,last_name,email,password) 
@@ -26,11 +27,43 @@ const verifyEmail = (db, email) => {
     return db.query(query, values);
 }
 
+const getEmailById = (db, id) => {
+    const query = `SELECT email from users
+    where id = $1`;
+    const values = [id];
+    return db.query(query, values);
+}
+
 
 module.exports = (db) => {
     router.get("", (req, res) => {
         res.send(req.session.user_id);
     });
+
+    router.get("/authenticate", (req, res) => {
+        getEmailById(db, req.session.user_id)
+            .then((data) => {
+                if (data.rows.length === 0) {
+                    res.send({ message: 'no user' });
+                    return;
+                }
+                res.send({ email: data.rows[0].email });
+                return;
+            })
+    })
+
+    router.post("/login/dev", (req, res) => {
+        getEmailById(db, req.body.user_id)
+            .then((data) => {
+                if (data.rows.length === 0) {
+                    res.send({ message: 'no user' });
+                    return;
+                }
+                req.session.user_id = req.body.user_id;
+                res.send({ message: 'logged in!', email: data.rows[0].email });
+                return;
+            })
+    })
 
     router.post("/login", (req, res) => {
         verifyEmail(db, req.body.email)
@@ -48,7 +81,6 @@ module.exports = (db) => {
                         req.session.user_id = data.rows[0].id;
                         res.send('logged in!');
                         return;
-
                     })
             });
     })
