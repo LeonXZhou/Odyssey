79a1f185f9955f93cf77af16a29695bc91c591e3
LeonXZhou <leon.zhou@queensu.ca>
2022-01-17 01:13:10 -0500

    added post functionality

--- node-skeleton/server.js
+++ node-skeleton/server.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 
 // Web server config
 const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+
+const cookieSession = require('cookie-session');
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -18,25 +22,28 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.use(helmet());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['pi314', 'e278'],
+  maxAge: 24 * 60 * 60 * 1000,
+}))
 
-app.use(
-  "/styles",
-  sassMiddleware({
-    source: __dirname + "/styles",
-    destination: __dirname + "/public/styles",
-    isSass: false, // false => scss, true => sass
-  })
-);
 
+// app.use(bodyParser.urlencoded({ extended: true }));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const tripsRoutes = require("./routes/trips");
 const equipmentRoutes = require("./routes/equipment");
 const mealsRoutes = require("./routes/meals");
+const authentication = require("./routes/authentication");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -44,15 +51,15 @@ app.use("/api/users", usersRoutes(db));
 app.use("/api/equipment", equipmentRoutes(db));
 app.use("/api/plan-trips-meals", mealsRoutes(db));
 app.use("/api/trips", tripsRoutes(db));
+app.use("/", authentication(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/", (req, res) => {
-  res.send("i display here");
-});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
