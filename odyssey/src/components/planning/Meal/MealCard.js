9b617389a686ea748c107a29adf60393b1ff32d8
LeonXZhou <leon.zhou@queensu.ca>
2022-01-19 13:43:00 -0800

    implemented meals data formater and implemented meals display

--- odyssey/src/components/planning/Meal/MealCard.js
+++ odyssey/src/components/planning/Meal/MealCard.js
@@ -2,19 +2,22 @@ import React from "react";
 import "../../component-styles/MealCard.scss";
 import MealItem from "./MealItem";
 const MealCard = (props) => {
+  const mealItemArray =[];
+  for (const mealItemKey in props.mealState.mealItems)
+  {
+    mealItemArray.push(<MealItem key={mealItemKey} itemState={props.mealState.mealItems[mealItemKey]}></MealItem>)
+  }
   return (
     <div className={"mealCard"}>
-      <div className={"mealName"}><h1>Mean Name</h1></div>
+      <div className={"mealName"}><h1>{props.mealState.mealName}</h1></div>
       <table>
-        <tr>
-          <th>Quantity</th>
-          <th className={"foodName"}>Item Name</th>
-          <th></th>
-        </tr>
-        <MealItem></MealItem>
-        <MealItem></MealItem>
-        <MealItem></MealItem>
-        <MealItem></MealItem>
+        <tbody>
+          <tr>
+            <th>Quantity</th>
+            <th className={"foodName"}>Item Name</th>
+          </tr>
+            {mealItemArray}
+        </tbody>
       </table>
     </div>
   );
--- odyssey/src/components/planning/Meal/MealDay.js
+++ odyssey/src/components/planning/Meal/MealDay.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../../component-styles/MealDay.scss";
 import MealCard from "./MealCard";
+import AddButton from "../AddButton"
 
 const MealDay = (props) => {
+  const date = new Date(props.mealDayState.date)
+  const mealArray = [];
+  console.log(props.mealDayState)
+  for (const mealKey in props.mealDayState.meals) {
+    mealArray.push(<MealCard key={mealKey} mealState={props.mealDayState.meals[mealKey]}></MealCard>)
+  }
+
   return (
     <div className={"mealDay"}>
-      <div>Day 1</div>
-      <MealCard></MealCard>
-      <MealCard></MealCard>
+      <div>{date.toDateString()}</div>
+      {mealArray}
+      <AddButton/>
     </div>);
 };
 
