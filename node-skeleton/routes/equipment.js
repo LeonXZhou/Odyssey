5d404bd6b1ec35214936d882af8680f3b7e614ff
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-18 17:42:22 -0700

    working on routes

--- node-skeleton/routes/equipment.js
+++ node-skeleton/routes/equipment.js
@@ -1,22 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const getEquipmentById = (db,tripID) => {
+  const query = `SELECT gear_items.name AS gear_item, gear_items.quantity,gear_categories.name AS catergory, trips.id , trips.name
+  FROM gear_items
+  JOIN gear_categories on gear_items.gear_category_id=gear_categories.id
+  JOIN trips ON gear_categories.trip_id = trips.id
+  WHERE trips.id = $1;`;
+  const values = [tripID];
+  return db.query(query, values);
+};
+const insertEquipmentItem = (db, categoryId, name, quantity) => {
+  const query = `INSERT INOT gear_items (gear_category_id, name ,quantity)
+              VALUES ($2 ,$3 ,$4 )`;
+  const values = [db, categoryId, name, quantity];
+  return db.query(query, values);
+};
+// -----------------------------------TESTING
+const updateTripItems = (db,name,id) => {
+  const query = `UPDATE gear_categories
+              SET name= $2
+              WHERE id=$1`;
+  const values = [id ,name ]          
+  return db.query(query, values)
+};
+
+
+
+
+
+// req.body = {category:{name}, updatedItems{0:{name,quantity,itemId}} newItems={0:{name,quantity}}} items can have 0-N keys
+// -----------------------------------------------
 
 module.exports = (db) => {
   router.get("/:trip_id", (req, res) => {
-    const query = `SELECT gear_items.name AS gear_item, gear_items.quantity,gear_categories.name AS catergory, trips.id , trips.name
-    FROM gear_items
-    JOIN gear_categories on gear_items.gear_category_id=gear_categories.id
-    JOIN trips ON gear_categories.trip_id = trips.id
-    WHERE trips.id = $1;`
-    const values = [req.params.trip_id];
-    db.query(query, values)
+    getEquipmentById(db)
       .then((req) => {
-      
-        res.json(req.rows)
+        res.json(req.rows);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
+
+    router.post("/:trip_id/new_equipment", (req, res) => {
+      console.log(req);
+      insertEquipmentItem = (db, req.categoryId, req.name, req.quantity);
+    });
+    
+
+    router.post("/:trip_id/:categoryid", (req, res)=>{
+      console.log(req.body);
+    }) 
   });
   return router;
 };
--- node-skeleton/routes/meals.js
+++ node-skeleton/routes/meals.js
@@ -1,26 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
+const getAllMeals = (db)=>{
+const query = `SELECT  meals.id AS meals_id ,meals.name AS meal, meal_items.id AS meal_itemID,meal_items.name AS food_item, meal_items.quantity, days.date AS days_date , days.id AS days_id
+FROM meal_items
+JOIN meals on meal_items.meal_id=meals.id
+JOIN days on meals.day_id=days.id
+;
+`
+return db.query(query)
+}
+
+const insertIntoMeals = (db,id) =>{
+ const query = `INSERT INTO meals (day_id , name)
+                    VALUES(${values},lunch)`
+const values = [id]
+return db.query(query, values)
+}
 
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(
-      `SELECT  meals.id AS meals_id ,meals.name AS meal, meal_items.id AS meal_itemID,meal_items.name AS food_item, meal_items.quantity, days.date AS days_date , days.id AS days_id
-    FROM meal_items
-    JOIN meals on meal_items.meal_id=meals.id
-    JOIN days on meals.day_id=days.id
-    ;
-    `
-    )
+    getAllMeals(db)
+    
       .then((data) => {
         const allMeals = data.rows;
         console.log(allMeals);
         res.json(allMeals);
+        return;
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  router.post("/:trip_id/meals", (req, res) => {
+    
+   insertIntoMeals( db ,[req.params.trip_id] )
+      .then((req) => {
+      
+        res.json(req.rows)
+        return;
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
   return router;
+
 };
+
