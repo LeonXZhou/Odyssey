d801c0197a6bf1e1106f3ed82444da8a523c1479
Scott Hillier <scotthhillier@gmail.com>
2022-01-19 18:52:38 -0800

    add category feature working

--- odyssey/src/components/planning/Meal/MealDay.js
+++ odyssey/src/components/planning/Meal/MealDay.js
@@ -1,23 +1,34 @@
 import React from "react";
 import "../../component-styles/MealDay.scss";
 import MealCard from "./MealCard";
-import AddButton from "../AddButton"
+import AddButton from "../AddButton";
 import { newMealOnDay } from "../../../Helpers/apiHelpers";
 
 const MealDay = (props) => {
-  const date = new Date(props.mealDayState.date)
+  const date = new Date(props.mealDayState.date);
   const mealArray = [];
-  console.log(props.mealDayState)
+  console.log(props.mealDayState);
   for (const mealKey in props.mealDayState.meals) {
-    mealArray.push(<MealCard key={mealKey} mealState={props.mealDayState.meals[mealKey]}></MealCard>)
+    mealArray.push(
+      <MealCard
+        key={mealKey}
+        mealState={props.mealDayState.meals[mealKey]}
+      ></MealCard>
+    );
   }
 
   return (
     <div className={"mealDay"}>
       <div>{date.toDateString()}</div>
       {mealArray}
-      <AddButton addButtonType={'Meal'} day_id={props.mealDayState.dayId} onSubmit={newMealOnDay} setState={props.setMealState} />
-    </div>);
+      <AddButton
+        addButtonType={"Meal"}
+        day_id={props.mealDayState.dayId}
+        onSubmit={newMealOnDay}
+        setState={props.setMealState}
+      />
+    </div>
+  );
 };
 
 export default MealDay;
