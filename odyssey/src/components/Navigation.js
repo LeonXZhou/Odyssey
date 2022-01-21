a275467da4029ac2f67bf044745ac5acef1a15ed
Scott Hillier <scotthhillier@gmail.com>
2022-01-20 17:34:53 -0800

    homepage styling almost done

--- odyssey/src/components/Navigation.js
+++ odyssey/src/components/Navigation.js
@@ -2,17 +2,15 @@ import React from "react";
 import { useContext } from "react";
 import { authContext } from "./providers/AuthenticationProvider";
 import { Link } from "react-router-dom";
-import "./component-styles/navigation.scss";
+import "./component-styles/Navigation.scss";
+import odyssey_logo from "./images/odyssey_logo.png";
 
 const Navigation = (props) => {
   const { user, auth } = useContext(authContext);
   return (
     <main className="header-container">
       <div className="header-left">
-        <Link to="/" type="button" className="btn btn-default">
-          Home
-        </Link>
-        <Link to="/planning" type="button" className="btn btn-default">
+        <Link to="/planning" type="button" className="nav-button">
           Discover
         </Link>
         <Link
@@ -26,20 +24,26 @@ const Navigation = (props) => {
           About
         </Link>
       </div>
+      <div className="header-middle">
+        <Link to="/" type="button" className="btn btn-default">
+          <img className="logo" src={odyssey_logo} />
+          Odyssey
+        </Link>
+      </div>
       {auth ? (
-        <>
-          <div>{`logged in as ${user.email}`}</div>
+        <div className="header-right">
+          <div>{`${user.firstName}`}</div>
           <Link to="/logout" type="button" className="btn btn-default">
-            logout
+            Logout
           </Link>
-        </>
+        </div>
       ) : (
         <div className="header-right">
-          <Link to="/login" type="button" className="btn btn-default">
-            Login
+          <Link to="/register" type="button" className="signup">
+            Sign Up
           </Link>
-          <Link to="/register" type="button" className="btn btn-default">
-            Register
+          <Link to="/login" type="button" className="login">
+            Log In
           </Link>
         </div>
       )}
