912fdee6a955d068621d7b94a794b5009f5e6acf
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-21 18:50:08 -0700

    making queries to db

--- node-skeleton/db/queries/03_plan_trip_equipment.sql
+++ node-skeleton/db/queries/03_plan_trip_equipment.sql
@@ -11,3 +11,17 @@ FROM emergency_contacts
 WHERE message_sent = 'false';
 
 
+
+SELECT emergency_contacts.id , emergency_contacts.trip_id , emergency_contacts.name , emergency_contacts.email
+FROM emergency_contacts
+JOIN trips ON emergency_contacts.trip_id=trips.id;
+
+
+
+SELECT users.first_name , users.last_name , trips.name ,trips.description ,trips.start_date,trips.end_date, emergency_contacts.name AS emergency_contact, emergency_contacts.phone_number AS emergency_contact_phone,emergency_contacts.email AS emergency_contact_email, emergency_contacts.send_date
+FROM trip_owners
+JOIN  users ON trip_owners.user_id =users.id
+JOIN  trips ON trip_owners.trip_id =trips.id
+JOIN emergency_contacts ON trip_owners.trip_id =emergency_contacts.trip_id ;
+
+
