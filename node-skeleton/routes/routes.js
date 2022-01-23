d7db29aea09cb822493653a42e16c686b5a99ad5
Scott Hillier <scotthhillier@gmail.com>
2022-01-22 19:19:06 -0800

    pulling main

--- node-skeleton/routes/routes.js
+++ node-skeleton/routes/routes.js
@@ -7,15 +7,14 @@ function updateRoute(db, lat, lon, id) {
   return db.query(query, values);
 }
 module.exports = (db) => {
-    router.get("/:trip_id", (req, res) => {
-      console.log("I got the request");
-      getMealsForTrip(db, req.params.trip_id)
-        .then((data) => {
-          res.send(data.rows);
-        })
-        .catch((err) => {
-          res.status(500).json({ error: err.message });
-        });
-    });
-    return router
-}
+  router.get("/:trip_id", (req, res) => {
+    getMealsForTrip(db, req.params.trip_id)
+      .then((data) => {
+        res.send(data.rows);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
