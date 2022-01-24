ea3da97e44ae868cc0706552f1efd19bba2e27fe
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-24 13:29:14 -0700

    added some seeds

--- node-skeleton/db/schema/02_trips.sql
+++ node-skeleton/db/schema/02_trips.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS trips CASCADE;
 CREATE TABLE trips (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
+  description VARCHAR(500),
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   shared BOOLEAN DEFAULT false,
