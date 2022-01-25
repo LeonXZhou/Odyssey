1a5cda9770a5060d6c0b08bff5caf55e33076186
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-24 19:32:44 -0700

    updating seeds

--- node-skeleton/db/seeds/10_gear_items.sql
+++ node-skeleton/db/seeds/10_gear_items.sql
@@ -17,3 +17,25 @@ INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('stove',3,2);
 INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('pot',3,2);
 INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('pan',3,2);
 INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('spatula',3,2);
+
+
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('grill',6,1);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('pan',6,1);
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('sleeping bag',5,1);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('tent',5,1);
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('beanie',4,2);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('long sleeve shirt',4,4);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('jacket',4,2);
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('beanie',7,2);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('long sleeve shirt',7,4);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('jacket',7,2);
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('sleeping bag',8,1);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('tent',8,1);
+
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('grill',9,1);
+INSERT INTO gear_items (name,gear_category_id,quantity) VALUES ('pan',9,1);
