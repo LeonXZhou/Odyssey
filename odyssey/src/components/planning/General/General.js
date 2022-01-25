fc4fe5593b4b9a7a76fc0a7f76d41623ef38e2e1
Scott Hillier <scotthhillier@gmail.com>
2022-01-24 18:27:15 -0800

    public/private feature added to general section from sidebar

--- odyssey/src/components/planning/General/General.js
+++ odyssey/src/components/planning/General/General.js
@@ -1,40 +1,110 @@
-import { getMealsForTrip, updateGeneralForTrip } from "../../../Helpers/apiHelpers";
+import React, { useEffect, useState } from "react";
+import { Route, useParams } from "react-router";
+import {
+  getMealsForTrip,
+  updateGeneralForTrip,
+  updatePrivacyForTrip,
+  getPrivacyForTrip,
+} from "../../../Helpers/apiHelpers";
 import { formatTripMealsData } from "../../../Helpers/dataHelpers";
 export default function General(props) {
-    console.log(props.generalState)
-    let start = "";
-    let end = "";
-    if (props.generalState.start_date) {
-        start = new Date(props.generalState.start_date).toISOString().split('T')[0]
-    }
-    if (props.generalState.end_date) {
-        end = new Date(props.generalState.end_date).toISOString().split('T')[0]
-    }
-    return (
-        <main>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                updateGeneralForTrip(props.generalState.id, props.generalState)
-                    .then(() => {
-                        getMealsForTrip(props.generalState.id).then((res) => {
-                            props.setMealState(formatTripMealsData(res.data));
-                        });
-                    })
-            }}>
-                <label>Trip Name</label>
-                <input type={'text'}
-                    value={props.generalState.name ? props.generalState.name : ""}
-                    onChange={(e) => { props.setGeneralState((prev) => { return { ...prev, name: e.target.value } }) }}></input>
-                <label>descirption</label>
-                <textarea value={props.generalState.description ? props.generalState.description : ""}
-                    onChange={(e) => { props.setGeneralState((prev) => { return { ...prev, description: e.target.value} }) }}></textarea>
-                <label >State Date</label>
-                <input type="date" value={start}
-                    onChange={(e) => { props.setGeneralState((prev) => { return { ...prev, start_date: e.target.value+"T05:00:00.000Z" } }) }}></input>
-                <label>End Date</label>
-                <input type="date" value={end}
-                    onChange={(e) => { props.setGeneralState((prev) => { return { ...prev, end_date: e.target.value+"T05:00:00.000Z"  } }) }}></input>
-                <button>Update</button>
-            </form>
-        </main>)
+  const { trip_id } = useParams();
+  const [privacyState, setPrivacyState] = useState(false);
+  useEffect(() => {
+    getPrivacyForTrip(trip_id).then((res) => {
+      setPrivacyState(res.data[0].shared);
+    });
+  });
+  console.log(props.generalState);
+  let start = "";
+  let end = "";
+  if (props.generalState.start_date) {
+    start = new Date(props.generalState.start_date).toISOString().split("T")[0];
+  }
+  if (props.generalState.end_date) {
+    end = new Date(props.generalState.end_date).toISOString().split("T")[0];
+  }
+  return (
+    <main>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          updateGeneralForTrip(props.generalState.id, props.generalState).then(
+            () => {
+              getMealsForTrip(props.generalState.id).then((res) => {
+                props.setMealState(formatTripMealsData(res.data));
+              });
+            }
+          );
+        }}
+      >
+        <label>Trip Name</label>
+        <input
+          type={"text"}
+          value={props.generalState.name ? props.generalState.name : ""}
+          onChange={(e) => {
+            props.setGeneralState((prev) => {
+              return { ...prev, name: e.target.value };
+            });
+          }}
+        ></input>
+        <label>descirption</label>
+        <textarea
+          value={
+            props.generalState.description ? props.generalState.description : ""
+          }
+          onChange={(e) => {
+            props.setGeneralState((prev) => {
+              return { ...prev, description: e.target.value };
+            });
+          }}
+        ></textarea>
+        <label>State Date</label>
+        <input
+          type="date"
+          value={start}
+          onChange={(e) => {
+            props.setGeneralState((prev) => {
+              return { ...prev, start_date: e.target.value + "T05:00:00.000Z" };
+            });
+          }}
+        ></input>
+        <label>End Date</label>
+        <input
+          type="date"
+          value={end}
+          onChange={(e) => {
+            props.setGeneralState((prev) => {
+              return { ...prev, end_date: e.target.value + "T05:00:00.000Z" };
+            });
+          }}
+        ></input>
+        {privacyState === false && (
+          // trip is private
+          <button
+            className="sidebar-item"
+            onClick={() => {
+              setPrivacyState(true);
+              updatePrivacyForTrip(trip_id, true);
+            }}
+          >
+            Private
+          </button>
+        )}
+        {privacyState === true && (
+          // trip is public
+          <button
+            className="sidebar-item"
+            onClick={() => {
+              setPrivacyState(false);
+              updatePrivacyForTrip(trip_id, false);
+            }}
+          >
+            Public
+          </button>
+        )}
+        <button>Update</button>
+      </form>
+    </main>
+  );
 }
