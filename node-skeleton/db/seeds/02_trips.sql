fc668dcb0c234135e0c9e03e60b7ed207da1c493
Luis Andres Contreras <tremenditomen@gmail.com>
2022-01-25 18:46:26 -0700

    added tents and changed one route to lomas de arena

--- node-skeleton/db/seeds/02_trips.sql
+++ node-skeleton/db/seeds/02_trips.sql
@@ -2,4 +2,4 @@ INSERT INTO trips (name, description, start_date, end_date, creator, shared)
 VALUES
   ('Juan de Fuca trail', 'The Juan de Fuca Marine Trail covers 47 kilometres of dense, coastal forest. The trail carries alongside and over beautiful pebble beaches, waterfalls, suspension bridges, and the occasional pit of mud.', '2022-01-27', '2022-02-03', 1,true),
   ('Nakahechi trail in Kumano Kodo', 'The Nakahechi is the core trail of the Kumano Kodo pilgrimage network with a history of over 1000 years. It can be walked in consecutive day treks or experienced with shorter highlight walks. It is a mountain environment so if your planning to do a multi-day walk please be prepared.', '2022-01-27', '2022-02-01', 2 , true),
-  ('El Choro Trek', 'El Choro Trek is a 68.5 mile lightly trafficked out and back trail located near La Paz, La Paz, Bolivia that features a lake. The trail is rated as difficult and is primarily used for hiking, camping, nature trips, and backpacking.', '2022-01-28', '2022-02-02', 2, true);
+  ('Lomas de Arena', '34,594-acre reserve featuring lagoons & sand dunes, plus trails, wildlife & horseback rides.', '2022-01-28', '2022-02-02', 2, true);
